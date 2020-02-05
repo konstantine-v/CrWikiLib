145ef7e6e61e4a3a73835f31c6eeb917bfb87bc9
Konstantine <konstantinev@protonmail.com>
2020-02-05 16:50:47 -0600

    Updated page file

--- src/wiki-api/page.cr
+++ src/wiki-api/page.cr
@@ -1,6 +1,4 @@
 class Wiki::Page
-  @@api_url   = "w/api.php"
-
   def self.new(proto : String, query : String)
     params = {
       "format"    => ["json"],
@@ -10,18 +8,17 @@ class Wiki::Page
       "titles"    => [query],
     }
     req_params = HTTP::Params.new(params)
-    response = HTTP::Client.get("#{proto}://#{Wiki::URL}/#{@@api_url}?#{req_params}")
+    response = HTTP::Client.get("#{proto}://#{Wiki::API_URL}?#{req_params}")
     raise Exceptions::Generic.new("Something went wrong") unless response.success?
     return response.body
   end
 
   def self.pdf(proto : String, title : String)
     path_url   = "api/rest_v1/page/pdf"
-    pdf_url    = "#{proto}://#{Wiki::URL}/#{path_url}/#{title}"
+    pdf_url    = "#{proto}://#{Wiki::API_URL}/#{title}"
 
     response = HTTP::Client.get(pdf_url)
     raise Exceptions::Generic.new("Something went wrong") unless response.success?
     return response
   end
-
 end
